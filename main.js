8c3e28b4b0a7a7b011faceef470d8a871d3e4ff1
Yarynochka <jaryna.games@gmail.com>
2022-04-14 18:55:51 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,82 @@
+// let a = 'hello';
+// // console.log(a);
+//
+// let b = 'owu';
+// // console.log(b);
+//
+// let c = 'com';
+// let d = 'ua';
+// console.log(a +' '+ b+' '+ c+' '+ d);
+//
+// let e = 1;
+// let f = 10;
+// let g = -999;
+// let s = 123;
+// let p = 3.14;
+// let bool = 8>9;
+// let bool1 = 8<9;
+//
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(s);
+// console.log(p);
+// console.log(bool);
+// console.log(bool1);
+
+// let book1= {
+//     name: 'milk with blood',
+//     pages: 210,
+//     genre: 'novel'
+// };
+//
+// let book2= {
+//     name: 'Initiation',
+//     pages: 210,
+//     genre: 'novel',
+//     author:['luko-dashvar','author2']
+// };
+// console.log(book2.author[0]);
+
+let firstName = 'Lupiy ';
+let middleName = 'Yaryna ';
+let lastName = 'Andriyvna';
+console.log(firstName+' '+ middleName+' '+ lastName);
+
+let person = firstName+middleName+lastName;
+console.log(person);
+
+// var hi = prompt('What is your name?','');
+// alert('Hello '+ hi+'!');
+
+// let n = Number(prompt('enter your age:','0'));
+// alert('Hello ' + firstName+ middleName+ n);
+
+// window.name='test';
+// alert(window.name);
+
+let a =100;
+console.log(typeof a);
+
+let b = '100';
+console.log(typeof b);
+
+let c = true;
+console.log(typeof c);
+
+let books= [
+    {name:'book1',page: 42, genre: 'genre1', author: 'author1'},
+    {name:'book2',page: 45, genre: 'genre2', author: 'author2'},
+    {name: 'kjfv',page: 53, genre: 'genre3', author: 'author3'},
+    {name:'book4',page: 100, genre: 'genre4', author: 'author4'},
+
+];
+console.log(books[0])
+console.log(books[1])
+console.log(books[2].page)
+console.log(books[3])
+
+
+
+
+
